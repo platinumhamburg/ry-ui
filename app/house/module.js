317e55c09fd03db3606688aed1cd7122f6d0fb55
ocean.wy <platinumhamburg@icloud.com>
2017-01-16 21:46:56 +0800

    new

--- app/house/module.js
+++ app/house/module.js
@@ -1,7 +1,7 @@
 "use strict";
 
 
-angular.module('app.house', ['ui.router','uiGmapgoogle-maps'])
+angular.module('app.house', ['ui.router','ngResource'])
 .config(function ($stateProvider) {
 
     $stateProvider
